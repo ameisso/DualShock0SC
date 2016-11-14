e24ffe6fff6b7ab796748b6857bca323666dbf53
Antoine <ameisso@gmail.com>
2016-11-14 02:26:27 +0100

    Initial commit

--- Dualshock4HID/AppDelegate.h
+++ Dualshock4HID/AppDelegate.h
@@ -7,8 +7,107 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <DDHidLib/DDHidLib.h>
+#import <VVOSC/VVOSC.h>
+
+#define SQUARE_BUTTON 0
+#define CROSS_BUTTON 1
+#define CIRCLE_BUTTON 2
+#define TRIANGLE_BUTTON 3
+
+#define L1_BUTTON 4
+#define R1_BUTTON 5
+#define L2_BUTTON 6
+#define R2_BUTTON 7
+
+#define SHARE_BUTTON 8
+#define OPTION_BUTTON 9
+
+#define LEFT_HAT_BUTTON 10
+#define RIGHT_HAT_BUTTON 11
+
+#define PS_BUTTON 12
+#define PAD_BUTTON 13
+
+#define L2_JOYSTICK 2
+#define R2_JOYSTICK 3
+
+
+#define RIGHT_JOYSTICK_X 0
+#define RIGHT_JOYSTICK_Y 1
+
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+    NSArray *joysticks;
+    OSCManager *oscManager;
+    OSCOutPort *outPort;
+    
+    IBOutlet NSPopUpButton *oscTargetPopUpButton;
+    IBOutlet NSTextField *triangleTextField;
+    IBOutlet NSTextField *circleTextField;
+    IBOutlet NSTextField *crossTextField;
+    IBOutlet NSTextField *squareTextField;
+    
+    IBOutlet NSTextField *topTextField;
+    IBOutlet NSTextField *rightTextField;
+    IBOutlet NSTextField *bottomTextField;
+    IBOutlet NSTextField *leftTextField;
+    
+    IBOutlet NSTextField *L1TextField;
+    IBOutlet NSTextField *L2TextField;
+    IBOutlet NSTextField *R1TextField;
+    IBOutlet NSTextField *R2TextField;
+    
+    IBOutlet NSTextField *LeftHatButtonTextField;
+    IBOutlet NSTextField *RightHatButtonTextField;
+    
+    IBOutlet NSTextField *L2JoysticktextField;
+    IBOutlet NSTextField *R2JoysticktextField;
+    
+    IBOutlet NSTextField *LHatJoysticktextField;
+    IBOutlet NSTextField *RHatJoysticktextField;
+    
+    IBOutlet NSImageView *outPortImageView;
+    
+    IBOutlet NSImageView *triangleImageView;
+    IBOutlet NSImageView *circleImageView;
+    IBOutlet NSImageView *crossImageView;
+    IBOutlet NSImageView *squareImageView;
+    
+    IBOutlet NSImageView *topImageView;
+    IBOutlet NSImageView *rightImageView;
+    IBOutlet NSImageView *bottomImageView;
+    IBOutlet NSImageView *leftImageView;
+    
+    IBOutlet NSImageView *L1ImageView;
+    IBOutlet NSImageView *L2tImageView;
+    IBOutlet NSImageView *R1ImageView;
+    IBOutlet NSImageView *R2ImageView;
+    
+    IBOutlet NSImageView *LeftHatButtonImageView;
+    IBOutlet NSImageView *RightHatButtonImageView;
+    
+    IBOutlet NSImageView *L2JoystickImageView;
+    IBOutlet NSImageView *R2JoystickImageView;
+    
+    IBOutlet NSImageView *LHatJoystickImageView;
+    IBOutlet NSImageView *RHatJoystickImageView;
+    
+    BOOL isTopPressed;
+    BOOL isLeftPressed;
+    BOOL isBottomPressed;
+    BOOL isRightPressed;
+    
+    NSSize leftJoystickSize;
+    NSSize rightJoystickSize;
+}
+
+- (void) ddhidJoystick: (DDHidJoystick *)  joystick stick: (unsigned) stick xChanged: (int) value;
+- (void) ddhidJoystick: (DDHidJoystick *)  joystick stick: (unsigned) stick yChanged: (int) value;
+- (void) ddhidJoystick: (DDHidJoystick *) joystick stick: (unsigned) stick otherAxis: (unsigned) otherAxis valueChanged: (int) value;
+- (void) ddhidJoystick: (DDHidJoystick *) joystick buttonDown: (unsigned) buttonNumber;
+- (void) ddhidJoystick: (DDHidJoystick *) joystick buttonUp: (unsigned) buttonNumber;
 
 
 @end
--- Dualshock4HID/AppDelegate.m
+++ Dualshock4HID/AppDelegate.m
@@ -1,12 +1,5 @@
-//
-//  AppDelegate.m
-//  Dualshock4HID
-//
-//  Created by Antoine on 13/11/16.
-//  Copyright © 2016 Antoine. All rights reserved.
-//
-
 #import "AppDelegate.h"
+
 
 @interface AppDelegate ()
 
@@ -16,13 +9,474 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    joysticks = [DDHidJoystick allJoysticks];
+    [joysticks makeObjectsPerformSelector: @selector(setDelegate:) withObject: self];
+    for( DDHidJoystick *joystick in joysticks )
+    {
+        NSLog(@"%@ joysticks %d buttons %d",joystick.productName,joystick.countOfSticks,joystick.numberOfButtons);
+        [joystick startListening];
+    }
+    
+    [self createOSC];
+    [self load];
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+#pragma mark OSC
+- (void)createOSC
+{
+    [outPortImageView setImage:self.errorImage];
+    oscManager = [[OSCManager alloc] init];
+    [oscManager setDelegate:self];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(oscOutputsChangedNotification:) name:OSCOutPortsChangedNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(oscOutputsChangedNotification:) name:OSCInPortsChangedNotification object:nil];
+    
+    [self oscOutputsChangedNotification:nil];
+}
+
+- (void) oscOutputsChangedNotification:(NSNotification *)notification
+{
+    [oscTargetPopUpButton removeAllItems];
+    NSArray *portLabelArray = [oscManager outPortLabelArray];
+    [oscTargetPopUpButton addItemsWithTitles:portLabelArray];
+    
+    NSString *portLabel = [[[NSUserDefaults standardUserDefaults] objectForKey:@"outPortSnapshot"] objectForKey:@"portLabel"];
+    
+    outPort = [oscManager findOutputWithLabel:portLabel];
+    [oscTargetPopUpButton selectItemWithTitle:portLabel];
+}
+
+- (IBAction)targetButtonChanged:(id)sender
+{
+    int	selectedIndex = (int)[oscTargetPopUpButton indexOfSelectedItem];
+    outPort = nil;
+    [outPortImageView setImage:self.errorImage];
+    if (selectedIndex == -1)
+    {
+        return;
+    }
+    outPort = [oscManager findOutputForIndex:selectedIndex];
+    [[NSUserDefaults standardUserDefaults] setObject:outPort.createSnapshot forKey:@"outPortSnapshot"];
+}
+
+- (void)sendMessageWithAddress:(NSString*)address andValue:(int)value
+{
+    OSCMessage *oscMessage = [OSCMessage createWithAddress:address];
+    [oscMessage addInt:value];
+    [outPort sendThisMessage:oscMessage];
+    if (outPort)
+    {
+        [outPortImageView setImage:self.pressedImage];
+    }
+    else
+    {
+        [outPortImageView setImage:self.errorImage];
+    }
+    
+    [self save];
+}
+
+- (void)sendMessageWithAddress:(NSString*)address andFloatValue:(float)value
+{
+    OSCMessage *oscMessage = [OSCMessage createWithAddress:address];
+    [oscMessage addFloat:value];
+    [outPort sendThisMessage:oscMessage];
+}
+
+- (void)sendMessageWithAddress:(NSString*)address XValue:(float) xValue andYValue:(float)yValue
+{
+    OSCMessage *oscMessage = [OSCMessage createWithAddress:address];
+    [oscMessage addFloat:xValue];
+    [oscMessage addFloat:yValue];
+    [outPort sendThisMessage:oscMessage];
+    
+    NSString *xAddress = [NSString stringWithFormat:@"%@/x",address];
+    OSCMessage *xMessage = [OSCMessage createWithAddress:xAddress];
+    [xMessage addFloat:xValue];
+    [outPort sendThisMessage:xMessage];
+    
+    NSString *yAddress = [NSString stringWithFormat:@"%@/y",address];
+    OSCMessage *yMessage = [OSCMessage createWithAddress:yAddress];
+    [yMessage addFloat:yValue];
+    [outPort sendThisMessage:yMessage];
+    [self updateJoysticksImages];
+}
+
+#pragma mark JOYSTICKS
+- (NSImage*)pressedImage
+{
+    return [NSImage imageNamed:@"NSStatusAvailable"];
+}
+
+- (NSImage*)releasedImage
+{
+    return [NSImage imageNamed:@"NSStatusNone"];
+}
+
+- (NSImage*)errorImage
+{
+    return [NSImage imageNamed:@"NSStatusUnavailable"];
+}
+
+- (void) ddhidJoystick: (DDHidJoystick *) joystick buttonDown: (unsigned) buttonNumber;
+{
+    switch (buttonNumber)
+    {
+        case TRIANGLE_BUTTON:
+            [triangleImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:triangleTextField.stringValue andValue:1];
+            break;
+            
+        case CIRCLE_BUTTON:
+            [circleImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:circleTextField.stringValue andValue:1];
+            break;
+            
+        case CROSS_BUTTON:
+            [crossImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:crossTextField.stringValue andValue:1];
+            break;
+            
+        case SQUARE_BUTTON:
+            [squareImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:squareTextField.stringValue andValue:1];
+            break;
+            
+        case L1_BUTTON:
+            [L1ImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:L1TextField.stringValue andValue:1];
+            break;
+            
+        case L2_BUTTON:
+            [L2tImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:L2TextField.stringValue andValue:1];
+            break;
+            
+            
+        case R1_BUTTON:
+            [R1ImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:R1TextField.stringValue andValue:1];
+            break;
+            
+        case R2_BUTTON:
+            [R2ImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:R2TextField.stringValue andValue:1];
+            break;
+            
+        case LEFT_HAT_BUTTON:
+            [LeftHatButtonImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:LeftHatButtonTextField.stringValue andValue:1];
+            break;
+            
+        case RIGHT_HAT_BUTTON:
+            [RightHatButtonImageView setImage:[self pressedImage]];
+            [self sendMessageWithAddress:RightHatButtonTextField.stringValue andValue:1];
+            break;
+            
+            
+        default:
+            break;
+    }
+}
+
+- (void)ddhidJoystick:(DDHidJoystick *)joystick buttonUp:(unsigned int)buttonNumber
+{
+    switch (buttonNumber)
+    {
+        case TRIANGLE_BUTTON:
+            [triangleImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:triangleTextField.stringValue andValue:0];
+            break;
+            
+        case CIRCLE_BUTTON:
+            [circleImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:circleTextField.stringValue andValue:0];
+            break;
+            
+        case CROSS_BUTTON:
+            [crossImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:crossTextField.stringValue andValue:0];
+            break;
+            
+        case SQUARE_BUTTON:
+            [squareImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:squareTextField.stringValue andValue:0];
+            break;
+            
+        case L1_BUTTON:
+            [L1ImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:L1TextField.stringValue andValue:0];
+            break;
+            
+        case L2_BUTTON:
+            [L2tImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:L2TextField.stringValue andValue:0];
+            break;
+            
+            
+        case R1_BUTTON:
+            [R1ImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:R1TextField.stringValue andValue:0];
+            break;
+            
+        case R2_BUTTON:
+            [R2ImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:R2TextField.stringValue andValue:0];
+            break;
+            
+        case LEFT_HAT_BUTTON:
+            [LeftHatButtonImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:LeftHatButtonTextField.stringValue andValue:0];
+            break;
+            
+        case RIGHT_HAT_BUTTON:
+            [RightHatButtonImageView setImage:[self releasedImage]];
+            [self sendMessageWithAddress:RightHatButtonTextField.stringValue andValue:0];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void) ddhidJoystick: (DDHidJoystick *)  joystick stick: (unsigned) stick xChanged: (int) value
+{
+    float mappedValue = [self map:value inMin:-65536 inMax:65536 outMin:0 outMax:1];
+    leftJoystickSize.width = mappedValue;
+    [self sendMessageWithAddress:LHatJoysticktextField.stringValue XValue:leftJoystickSize.width andYValue:leftJoystickSize.height];
+}
+
+- (void) ddhidJoystick: (DDHidJoystick *)  joystick stick: (unsigned) stick yChanged: (int) value
+{
+    float mappedValue = [self map:value inMin:-65536 inMax:65536 outMin:0 outMax:1];
+    leftJoystickSize.height = mappedValue;
+    [self sendMessageWithAddress:LHatJoysticktextField.stringValue XValue:leftJoystickSize.width andYValue:leftJoystickSize.height];
+}
+
+- (void) ddhidJoystick: (DDHidJoystick *) joystick stick: (unsigned) stick otherAxis: (unsigned) otherAxis valueChanged: (int) value;
+{    
+    BOOL shouldSendRightJoystick = NO;
+    float mappedValue = [self map:value inMin:-65536 inMax:65536 outMin:0 outMax:1];
+    switch (otherAxis)
+    {
+        case L2_JOYSTICK:
+            if(value == -65536)
+            {
+                [L2JoystickImageView setImage:self.releasedImage];
+            }
+            else
+            {
+                [L2JoystickImageView setImage:self.pressedImage];
+            }
+            [self sendMessageWithAddress:L2JoysticktextField.stringValue andFloatValue:mappedValue];
+            break;
+        case R2_JOYSTICK:
+            if(value == -65536)
+            {
+                [R2JoystickImageView setImage:self.releasedImage];
+            }
+            else
+            {
+                [R2JoystickImageView setImage:self.pressedImage];
+            }
+            [self sendMessageWithAddress:R2JoysticktextField.stringValue andFloatValue:mappedValue];
+            break;
+        case RIGHT_JOYSTICK_X:
+            rightJoystickSize.width = mappedValue;
+            shouldSendRightJoystick = YES;
+            break;
+        case RIGHT_JOYSTICK_Y:
+            rightJoystickSize.height = mappedValue;
+            shouldSendRightJoystick = YES;
+            break;
+            
+            
+        default:
+            break;
+    }
+    if(shouldSendRightJoystick)
+    {
+        [self sendMessageWithAddress:RHatJoysticktextField.stringValue XValue:rightJoystickSize.width andYValue:rightJoystickSize.height];
+    }
+}
+
+- (void) ddhidJoystick: (DDHidJoystick *) joystick stick: (unsigned) stick povNumber: (unsigned) povNumber valueChanged: (int) value;
+{
+    switch (value)
+    {
+        case 0:
+            isTopPressed = YES;
+            break;
+        case 4500:
+            isTopPressed = YES;
+            isRightPressed = YES;
+            break;
+        case 9000:
+            isRightPressed = YES;
+            break;
+        case 13500:
+            isRightPressed = YES;
+            isBottomPressed = YES;
+            break;
+        case 18000:
+            isBottomPressed = YES;
+            break;
+        case 22500:
+            isBottomPressed = YES;
+            isLeftPressed = YES;
+            break;
+        case 27000:
+            isLeftPressed = YES;
+            break;
+        case 31500:
+            isLeftPressed = YES;
+            isTopPressed = YES;
+            break;
+        default: //-1 release
+            if (isTopPressed)
+            {
+                [self sendMessageWithAddress:topTextField.stringValue andValue:0];
+                [topImageView setImage:self.releasedImage];
+                isTopPressed = NO;
+            }
+            if (isRightPressed)
+            {
+                [self sendMessageWithAddress:rightTextField.stringValue andValue:0];
+                [rightImageView setImage:self.releasedImage];
+                isRightPressed = NO;
+            }
+            if (isBottomPressed)
+            {
+                [self sendMessageWithAddress:bottomTextField.stringValue andValue:0];
+                [bottomImageView setImage:self.releasedImage];
+                isBottomPressed = NO;
+            }
+            if (isLeftPressed)
+            {
+                [self sendMessageWithAddress:leftTextField.stringValue andValue:0];
+                [leftImageView setImage:self.releasedImage];
+                isLeftPressed = NO;
+            }
+            break;
+    }
+    if (isTopPressed)
+    {
+        [self sendMessageWithAddress:topTextField.stringValue andValue:1];
+        [topImageView setImage:self.pressedImage];
+    }
+    if (isRightPressed)
+    {
+        [self sendMessageWithAddress:rightTextField.stringValue andValue:1];
+        [rightImageView setImage:self.pressedImage];
+    }
+    if (isBottomPressed)
+    {
+        [self sendMessageWithAddress:bottomTextField.stringValue andValue:1];
+        [bottomImageView setImage:self.pressedImage];
+    }
+    if (isLeftPressed)
+    {
+        [self sendMessageWithAddress:leftTextField.stringValue andValue:1];
+        [leftImageView setImage:self.pressedImage];
+    }
+}
+
+- (void)updateJoysticksImages
+{
+    float unMappedLeftX = [self map:leftJoystickSize.width inMin:0 inMax:1 outMin:-65536 outMax:65536];
+    float unMappedLeftY = [self map:leftJoystickSize.width inMin:0 inMax:1 outMin:-65536 outMax:65536];
+    if(unMappedLeftX > -3000 && unMappedLeftX < 3000 && unMappedLeftY > -3000 && unMappedLeftY < 3000)
+    {
+        [LHatJoystickImageView setImage:self.releasedImage];
+    }
+    else
+    {
+        [LHatJoystickImageView setImage:self.pressedImage];
+    }
+    
+    float unMappedRightX = [self map:rightJoystickSize.width inMin:0 inMax:1 outMin:-65536 outMax:65536];
+    float unMappedRightY = [self map:rightJoystickSize.width inMin:0 inMax:1 outMin:-65536 outMax:65536];
+    if(unMappedRightX > -3000 && unMappedRightX < 3000 && unMappedRightY > -3000 && unMappedRightY < 3000)
+    {
+        [RHatJoystickImageView setImage:self.releasedImage];
+    }
+    else
+    {
+        [RHatJoystickImageView setImage:self.pressedImage];
+    }
+    
+}
+#pragma mark CODING
+- (void)save
+{
+    [[NSUserDefaults standardUserDefaults] setObject:triangleTextField.stringValue forKey:@"triangleTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:circleTextField.stringValue forKey:@"circleTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:crossTextField.stringValue forKey:@"crossTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:squareTextField.stringValue forKey:@"squareTextField"];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:topTextField.stringValue forKey:@"topTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:rightTextField.stringValue forKey:@"rightTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:bottomTextField.stringValue forKey:@"bottomTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:leftTextField.stringValue forKey:@"leftTextField"];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:L1TextField.stringValue forKey:@"L1TextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:L2TextField.stringValue forKey:@"L2TextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:R1TextField.stringValue forKey:@"R1TextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:R2TextField.stringValue forKey:@"R2TextField"];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:LeftHatButtonTextField.stringValue forKey:@"LeftHatButtonTextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:RightHatButtonTextField.stringValue forKey:@"RightHatButtonTextField"];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:L2JoysticktextField.stringValue forKey:@"L2JoysticktextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:R2JoysticktextField.stringValue forKey:@"R2JoysticktextField"];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:LHatJoysticktextField.stringValue forKey:@"LHatJoysticktextField"];
+    [[NSUserDefaults standardUserDefaults] setObject:RHatJoysticktextField.stringValue forKey:@"RHatJoysticktextField"];
+    
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (void)load
+{
+    triangleTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"triangleTextField"];
+    circleTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"circleTextField"];
+    crossTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"crossTextField"];
+    squareTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"squareTextField"];
+    
+    topTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"topTextField"];
+    rightTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"rightTextField"];
+    bottomTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"bottomTextField"];
+    leftTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"leftTextField"];
+    
+    L1TextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"L1TextField"];
+    L2TextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"L2TextField"];
+    R1TextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"R1TextField"];
+    R2TextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"R2TextField"];
+    
+    LeftHatButtonTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"LeftHatButtonTextField"];
+    RightHatButtonTextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"RightHatButtonTextField"];
+    
+    L2JoysticktextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"L2JoysticktextField"];
+    R2JoysticktextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"R2JoysticktextField"];
+    
+    LHatJoysticktextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"LHatJoysticktextField"];
+    RHatJoysticktextField.stringValue = [[NSUserDefaults standardUserDefaults] stringForKey:@"RHatJoysticktextField"];
+}
+
+
+#pragma mark UTILS
+
+- (float)map:(float)value inMin:(float)inMin inMax:(float)inMax outMin:(float)outMin outMax:(float)outMax
+{
+    if( inMin == inMax )
+    {
+        return outMin;
+    }
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
 
 @end
